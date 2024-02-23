8e4debe4f924b4087f78405628dac5e9ebfb7b7d
venkataraman <venkat@ruvivo.com>
2024-02-23 09:23:45 +0530

    Delete unit testing

--- src/components/footer/__test__/footer.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import Footer from "../footer";
-
-describe("Footer Menu", () => {
-	test("Load Footer menu", () => {
-		render(<Footer />)
-	})
-})
